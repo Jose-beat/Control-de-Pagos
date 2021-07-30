8be4774c78de2b48cb75314e8f71ab25902f600d
Jose Uriel Rodriguez Ramirez <uri_055@hotmail.com>
2021-07-30 11:03:16 -0500

    'tipo_Pago_app'

---
+++ templates/test/sidebar.js
@@ -0,0 +1,6 @@
+function openNav(){
+    document.getElementById("sidebar").style.width = "25%"
+}
+function closeNav(){
+    document.getElementById("sidebar").style.width = "5%"
+}
---
+++ tipo_de_pago/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TipoDePagoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tipo_de_pago'
